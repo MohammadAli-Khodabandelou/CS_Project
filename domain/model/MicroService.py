5dfb6579b081a728b16a69a256439462081c5eea
mtndaghyani <matin.daghyani@gmail.com>
2022-07-19 12:01:57 +0430

    Add some helping method to microservice

--- domain/model/MicroService.py
+++ domain/model/MicroService.py
@@ -1,25 +1,54 @@
+from domain.model.queue import Queue
+from domain.model.request import Request
 from domain.model.service import Service
 
 
 class MicroService:
-    def __init__(self, service_type, service_time_mean: int, error_rate: float, instance_count=1):
-        self.input_queues = []
-        self.output_queues = []
+    def __init__(self, service_type,
+                 service_time_mean: int,
+                 error_rate: float,
+                 instance_count=1):
+        self.queue = Queue()
+        self.next_microservices = []
         self.service_type = service_type
         self.instance_count = instance_count
-        self.instances = self.make_instances(service_time_mean, error_rate)
+        self.instances = self._make_instances(service_time_mean, error_rate)
 
-    def make_instances(self, mean, error_rate):
+    def _make_instances(self, mean, error_rate):
         result = []
         for i in range(self.instance_count):
             result.append(Service(self.service_type, mean, error_rate))
         return result
 
-    def add_input_queue(self, queue):
-        self.input_queues.append(queue)
+    def _get_idle_instance(self):
+        # Returns an idle instance if exists, otherwise None
+        for s in self.instances:
+            if s.is_idle():
+                return s
+        return None
 
-    def add_output_queue(self, queue):
-        self.output_queues.append(queue)
+    def _process_request(self):
+        # Checks if there is an idle instance, then pass a request to the service
+        idle_service = self._get_idle_instance()
+        if idle_service is not None:
+            request = self.queue.get_request()
+            idle_service.set_request(request)
 
-    def _idle_service_exist(self):
+    def _update_instances(self):
+        # Checks each instance to see if it becomes idle. Passes the request to the next if necessary.
+        for s in self.instances:
+            s.update()
+            if s.timer.is_finished():
+                request = s.delete_request()
+                self._pass_request_to_next(request)
+
+    def add_next_microservice(self, microservice):
+        self.next_microservices.append(microservice)
+
+    def push_request(self, request: Request):
+        # Push a new request to the end of the queue
+        self.queue.add_request(request)
+        self._process_request()  # The added request must be processed if there is an idle instance
+
+    def _pass_request_to_next(self, request):
         pass
--- domain/model/service.py
+++ domain/model/service.py
@@ -1,13 +1,35 @@
 from domain.enumeration import EServiceType
 import numpy as np
 
+from domain.model.request import Request
+from domain.utils.timer import Timer
+
 
 class Service:
     def __init__(self, service_type: EServiceType, service_time_mean: int, error_rate: float):
         self.service_type = service_type
         self.service_time_mean = service_time_mean
         self.error_rate = error_rate
+        self.timer = Timer()
+        self.current_request = None
 
     def gen_service_time_teller1(self):
         return -np.log(1 - (np.random.uniform(low=0.0, high=1.0))) * self.service_time_mean
+
+    def is_idle(self):
+        return self.timer.is_finished()
+
+    def set_request(self, request: Request):
+        self.current_request = request
+        service_time = int(self.gen_service_time_teller1())
+        self.timer.set_period(service_time)
+
+    def delete_request(self):
+        r = self.current_request
+        self.current_request = None
+        return r
+
+    def update(self):
+        # Update the timer
+        self.timer.tick()
 
