4b12a5f1b68c0abc0565f74a6d69d8f37e7742c5
mtndaghyani <matin.daghyani@gmail.com>
2022-07-22 12:09:15 +0430

    Implement update_queue to handle requests timeout status

--- domain/model/queue.py
+++ domain/model/queue.py
@@ -9,6 +9,7 @@ class Queue:
         self.consumers = []
 
     def add_request(self, request: Request):
+        request.reset_timer()  # Reset the timeout timer of the new request.
         if request.order == 1:
             self.high_priority_queue.append(request)
         else:
@@ -23,3 +24,18 @@ class Queue:
         elif len(self.low_priority_queue) > 0:
             return self.low_priority_queue.pop(0)
         return None
+
+    def update_queue(self):
+        # This method checks if each request in the queue is in timeout status. If yes, removes from queue.
+        for i in range(len(self.high_priority_queue)):
+            r = self.high_priority_queue[i]
+            r.timer.tick()
+            if r.timer.is_finished():
+                self.high_priority_queue.pop(i)
+                # TODO: Change the status of the request to TIMEOUT ?
+        for i in range(len(self.low_priority_queue)):
+            r = self.high_priority_queue[i]
+            r.timer.tick()
+            if r.timer.is_finished():
+                self.high_priority_queue.pop(i)
+                # TODO: Change the status of the request to TIMEOUT ?
