6b2ff83f117af06654479be6afb88fc36076760d
mtndaghyani <matin.daghyani@gmail.com>
2022-07-26 23:52:58 +0430

    Add Analyzer.py

---
+++ domain/utils/Analyzer.py
@@ -0,0 +1,16 @@
+class Analyzer:
+    def __init__(self, all_requests, service_graph):
+        self.requests = all_requests
+        self.service_graph = service_graph
+
+    def analyze_mean_queue_length(self):
+        pass
+
+    def analyze_mean_waiting_time(self):
+        pass
+
+    def analyze_utilization(self):
+        pass
+
+    def analyze_timed_out_requests(self):
+        pass
